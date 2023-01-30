e35333a01882828f5f9006204f97cd5d16f2749f
Aaron Dreyer <aaronottaway@gmail.com>
2023-01-29 23:55:29 -0500

    Restructure
    I restructured my files to be more organized according to previous grading recommendations.

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,3 @@
-// function timeFresh() { 
-// var reformatDate = dayjs().format('dddd MMM D YYYY');
-// $('#currentDay').text(reformatDate);
-
-// }
-  
-//   setInterval(timeFresh, 1000);
-//   setTimeout(timeFresh, 0);
-
 var currentDay = $('#currentDay');
 
 function displayTime() {
@@ -17,30 +8,22 @@ function displayTime() {
 displayTime();
 setInterval(displayTime, 1000);
 
-var currentHour = parseInt(dayjs().hour());
+var currentHour = dayjs().hour();
 
 function colorCode() {
     $(".time-block").each(function () {
-    var currentBlock = parseInt($(this).attr("id").split("-")[1]);
+    var currentBlock = $(this).attr("id").split("-")[1];
     console.log("currentHour " + currentHour)
     console.log("currentBlock " + currentBlock)
 
     if (currentBlock < currentHour) {
-        console.log($(this).attr("class"))
         $(this).addClass("past");
-        console.log($(this).attr("class"))
-        console.log(currentBlock < currentHour)
-        console.log("past")
     }
     else if (currentBlock == currentHour) {
         $(this).addClass("present");
-        console.log(currentBlock == currentHour)
-        console.log("present")
     }
     else {
         $(this).addClass("future");
-        console.log(currentBlock > currentHour)
-        console.log("future")
     }
     })
 }
@@ -54,33 +37,14 @@ $(".saveBtn").on("click", function () {
   var time = $(this).parent().attr("id");
 
   localStorage.setItem(time, description);
-
 })
 
-// TO DO: not saving events entered into textarea after refresh
-
-
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+$("#hour-09 .description").val(localStorage.getItem("hour-09"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
